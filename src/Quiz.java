5ab58110999d93340ac1f59b3f5f595f808efa7f
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-13 17:09:41 -0700

    added questions and answers vars

--- src/Quiz.java
+++ src/Quiz.java
@@ -3,6 +3,20 @@ import java.awt.event.ActionListener;
 
 public class Quiz implements ActionListener {
 
+    String[] questions = {
+            "Which company created Java?",
+            "Which year was Java created?",
+            "What was Java originally called?"
+    };
+
+    String[][] options = {
+            {"Microsoft", "Sun Microsystems", "Starbucks", "Alphabet"},
+            {"1989", "1972", "1999", "1996"},
+            {"Mocha", "Apple", "Latte", "Oak"}
+    };
+
+
+
     public Quiz() {
 
     }
