f3cc2d6c850fe4b9fa43085221a71f916f850c30
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-13 17:04:48 -0700

    added empty methods

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,4 +1,27 @@
-public class Quiz {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Quiz implements ActionListener {
+
+    public Quiz() {
+
+    }
+
+    public void nextQuestion() {
+
+    }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void displayAnswer() {
+
+    }
+
+    public void results() {
+
+    }
 }
